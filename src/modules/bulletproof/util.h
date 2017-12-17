BlockstreamResearch/secp256k1-zkp

bulletproof: add module, support for rangeproofs including optimized verification

src/modules/bulletproof/util.h
@@ -1,0 +1,57 @@
+/**********************************************************************
+ * Copyright (c) 2017 Andrew Poelstra                                 *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef SECP256K1_MODULE_BULLETPROOF_UTIL
+#define SECP256K1_MODULE_BULLETPROOF_UTIL
+
+SECP256K1_INLINE static void secp256k1_bulletproof_serialize_points(unsigned char *out, secp256k1_ge *pt, size_t n) {
+    const size_t bitveclen = (n + 7) / 8;
+    size_t i;
+
+    memset(out, 0, bitveclen);
+    for (i = 0; i < n; i++) {
+        secp256k1_fe pointx;
+        pointx = pt[i].x;
+        secp256k1_fe_normalize(&pointx);
+        secp256k1_fe_get_b32(&out[bitveclen + i*32], &pointx);
+        if (!secp256k1_fe_is_quad_var(&pt[i].y)) {
+            out[i/8] |= (1ull << (i % 8));
+        }
+    }
+}
+
+SECP256K1_INLINE static void secp256k1_bulletproof_deserialize_point(secp256k1_ge *pt, const unsigned char *data, size_t i, size_t n) {
+    const size_t bitveclen = (n + 7) / 8;
+    const size_t offset = bitveclen + i*32;
+    secp256k1_fe fe;
+
+    secp256k1_fe_set_b32(&fe, &data[offset]);
+    secp256k1_ge_set_xquad(pt, &fe);
+    if (data[i / 8] & (1 << (i % 8))) {
+        secp256k1_ge_neg(pt, pt);
+    }
+}
+
+static void secp256k1_bulletproof_update_commit(unsigned char *commit, const secp256k1_ge *lpt, const secp256k1_ge *rpt) {
+    secp256k1_fe pointx;
+    secp256k1_sha256_t sha256;
+    unsigned char lrparity;
+    lrparity = (secp256k1_fe_is_quad_var(&lpt->y) >> 1) + secp256k1_fe_is_quad_var(&rpt->y);
+    secp256k1_sha256_initialize(&sha256);
+    secp256k1_sha256_write(&sha256, commit, 32);
+    secp256k1_sha256_write(&sha256, &lrparity, 1);
+    pointx = lpt->x;
+    secp256k1_fe_normalize(&pointx);
+    secp256k1_fe_get_b32(commit, &pointx);
+    secp256k1_sha256_write(&sha256, commit, 32);
+    pointx = rpt->x;
+    secp256k1_fe_normalize(&pointx);
+    secp256k1_fe_get_b32(commit, &pointx);
+    secp256k1_sha256_write(&sha256, commit, 32);
+    secp256k1_sha256_finalize(&sha256, commit);
+}
+
+#endif

afk11/secp256k1

src/modules/bulletproof/util.h
@@ -1,0 +1,57 @@
+/**********************************************************************
+ * Copyright (c) 2017 Andrew Poelstra                                 *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef SECP256K1_MODULE_BULLETPROOF_UTIL
+#define SECP256K1_MODULE_BULLETPROOF_UTIL
+
+SECP256K1_INLINE static void secp256k1_bulletproof_serialize_points(unsigned char *out, secp256k1_ge *pt, size_t n) {
+    const size_t bitveclen = (n + 7) / 8;
+    size_t i;
+
+    memset(out, 0, bitveclen);
+    for (i = 0; i < n; i++) {
+        secp256k1_fe pointx;
+        pointx = pt[i].x;
+        secp256k1_fe_normalize(&pointx);
+        secp256k1_fe_get_b32(&out[bitveclen + i*32], &pointx);
+        if (!secp256k1_fe_is_quad_var(&pt[i].y)) {
+            out[i/8] |= (1ull << (i % 8));
+        }
+    }
+}
+
+SECP256K1_INLINE static void secp256k1_bulletproof_deserialize_point(secp256k1_ge *pt, const unsigned char *data, size_t i, size_t n) {
+    const size_t bitveclen = (n + 7) / 8;
+    const size_t offset = bitveclen + i*32;
+    secp256k1_fe fe;
+
+    secp256k1_fe_set_b32(&fe, &data[offset]);
+    secp256k1_ge_set_xquad(pt, &fe);
+    if (data[i / 8] & (1 << (i % 8))) {
+        secp256k1_ge_neg(pt, pt);
+    }
+}
+
+static void secp256k1_bulletproof_update_commit(unsigned char *commit, const secp256k1_ge *lpt, const secp256k1_ge *rpt) {
+    secp256k1_fe pointx;
+    secp256k1_sha256 sha256;
+    unsigned char lrparity;
+    lrparity = (secp256k1_fe_is_quad_var(&lpt->y) >> 1) + secp256k1_fe_is_quad_var(&rpt->y);
+    secp256k1_sha256_initialize(&sha256);
+    secp256k1_sha256_write(&sha256, commit, 32);
+    secp256k1_sha256_write(&sha256, &lrparity, 1);
+    pointx = lpt->x;
+    secp256k1_fe_normalize(&pointx);
+    secp256k1_fe_get_b32(commit, &pointx);
+    secp256k1_sha256_write(&sha256, commit, 32);
+    pointx = rpt->x;
+    secp256k1_fe_normalize(&pointx);
+    secp256k1_fe_get_b32(commit, &pointx);
+    secp256k1_sha256_write(&sha256, commit, 32);
+    secp256k1_sha256_finalize(&sha256, commit);
+}
+
+#endif